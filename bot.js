cdc93d20906b85d479ba6c9b1cde938d49a4c11f
yellow <donte_beck@yahoo.com>
2019-01-21 22:47:10 -0700

    fennel cute

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const request = require('request');
 const fs = require('fs');
 const dateFormat = require('dateFormat');
 const JSON5 = require('json5');
-
 readJson('auth.json', (err, auth) => {
     client.login(auth.token);
     if(err) return errorLogger(err);
@@ -14,68 +13,144 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
+class Commands {
+    constructor() {
+        this.commands = {}
+    }
+    add(command, aliases, handler) {
+        this.commands[command] = handler;
+        for (let alias of aliases) {
+            this.commands[alias] = handler;
+        }
+    }
+    exists(command) {
+        return command in this.commands;
+    }
+    execute(command, message) {
+        this.commands[command](message);
+    }
+}
+
+const commands = new Commands();
+
+commands.add("3am", [], function(message) {
+    message.channel.send("fennel fennel cutie fennel stab me with ur rapier");
+    console.log(message.author.username + " wants fennel to stab them");
+});
+
+commands.add("fennel", [], function(message) {
+    message.channel.send("cute");
+    console.log(message.author.username + " knows fennel is cute");
+});
+
+commands.add("gacha", ["gachas"], function(message) {
+    readJson("json.json", function(err, data) {
+        if(err) return errLogger(err);
+        data = currentEntries(data, "Gacha");
+        data = data = data.map(entry => {
+            return `${entry.title[0]}: ${entry.timers[0].start} - ${entry.timers[0].end}`
+        });
+        console.log(data);
+        message.channel.send(data.join("\n") + "\n***All times JST** :flag_jp:");
+    });
+    console.log(message.author.username + " wants to know the current gachas");
+});
+
+commands.add("events", [], function(message) {
+    readJson("json.json", function(err, data) {
+        if(err) return errLogger(err);
+        data = currentEntries(data, "Event");
+        data = data.map(entry => {
+            return `${entry.title[0]}: ${entry.timers[0].start} - ${entry.timers[entry.timers.length-2].end}`
+        });
+        console.log(message.author.username + " wants to know the current event");
+        message.channel.send(data.join("\n") + "\n***All times JST** :flag_jp:");
+    });
+});
+
+commands.add("maintenance", ["maint"], function(message) {
+    readJson("json.json", function(err, data){
+        if(err) return errLogger(err);
+        data = currentEntries(data, "Maintenance");
+        data = data.map(entry =>{
+            return `${entry.title[0]}\n${entry.title[1]}: ${entry.timers[0].start} - ${entry.timers[0].end}`
+        });
+        console.log(message.author.username + " wants to know when the next maintenance is");
+        if (data.length > 0) {
+            message.channel.send(data.join("\n") + "\n***All times JST** :flag_jp:");
+        }
+        else message.channel.send("There are no scheduled maintenances.")
+    });
+});
+
+commands.add("eventshops", ["shops"], function(message) {
+    readJson("json.json", function(err, data) {
+        if(err) return errLogger(err);
+        let events = currentEntries(data, "Event")
+        let timers = [].concat.apply([], events.map(event => event.timers));
+        timers = timers.filter(timer => timer.name.startsWith("Event Shop"));
+        data = timers.map(entry => {
+            return `${entry.name}: ${entry.start} - ${entry.end}`
+        });
+        console.log(message.author.username + " wants to know the current event shops")
+        message.channel.send(data.join("\n") + "\n***All times JST** :flag_jp:");
+    });
+});
+
+commands.add("sales", ["sale"], function(message) {
+    readJson("json.json", function(err, data) {
+        if(err) return errLogger(err);
+        data = currentEntries(data, "Other");
+        data = data.filter(entry => entry.title[0].endsWith("Sale"))
+        data = data.map(entry =>{
+            return `- ${entry.title[0]}, ${entry.title[2]}:\n${entry.timers[0].start} - ${entry.timers[0].end}`
+        });
+        console.log(message.author.username + " wants to know the current sales");
+        if (data.length > 0) {
+            message.channel.send(data.join("\n") + "\n***All times JST** :flag_jp:");
+        }
+        else message.channel.send("There are no scheduled maintenances.")
+    });
+});
+
+commands.add("loginbonus", [], function(message) {
+    readJson("json.json", function(err, data) {
+        if(err) return errLogger(err);
+        data = currentEntries(data, "Other");
+        data = data.filter(entry => entry.title[0].endsWith("Login Bonus"));
+        data = data.map(entry => {
+            return `- ${entry.title[0]}, ${entry.title[2]}:\n${entry.timers[0].start} - ${entry.timers[0].end}`
+        });
+        console.log(message.author.username + " wants to know the current login bonuses");
+        if (data.length > 0) {
+            message.channel.send(data.join("\n") + "\n***All times JST** :flag_jp:");
+        }
+        else message.channel.send("There are no scheduled maintenances.")
+    });
+});
+
+commands.add("campaigns", ["campaign"], function(message) {
+    readJson("json.json", function(err, data) {
+        if(err) return errLogger(err);
+        data = currentEntries(data, "Other");
+        data = data.filter(entry => entry.title[0].endsWith("Campaign"));
+        data = data.map(entry =>{
+            return `- ${entry.title[0]}, ${entry.title[2]}:\n${entry.timers[0].start} - ${entry.timers[0].end}`
+        });
+        console.log(message.author.username + " wants to know the current campaigns");
+        if (data.length > 0) {
+            message.channel.send(data.join("\n") + "\n***All times JST** :flag_jp:");
+        }
+        else message.channel.send("There are no scheduled maintenances.")
+    });
+});
+
 client.on('message', message => {
-    let username = message.author.username;
-    if (message.content == "!fennel") {
-        message.channel.send("cute");
-        console.log(username + " knows fennel is cute");
+    if (message.content.startsWith("!")) {
+        let command = message.content.substring(1);
+        if(commands.exists(command)) commands.execute(command, message);
     }
-    if(message.content == "!3am") {
-        message.channel.send("fennel fennel cutie fennel stab me with ur rapier");
-        console.log(username + " wants fennel to stab them");
-    }
-    if(message.content == "!gachas" || message.content == "!gacha") {
-        readJson("json.json", function(err, data) {
-            if(err) return errLogger(err);
-            data = currentEntries(data, "Gacha");
-            data = data = data.map(entry => {
-                return `${entry.title[0]}: ${entry.timers[0].start} - ${entry.timers[0].end}`
-            });
-            console.log(data);
-            message.channel.send(data.join("\n") + "\n***All times JST** :flag_jp:");
-        });
-        console.log(username + " wants to know the current gachas");
-    }
-    if(message.content == "!events") {
-        readJson("json.json", function(err, data) {
-            if(err) return errLogger(err);
-            data = currentEntries(data, "Event");
-            data = data.map(entry => {
-                return `${entry.title[0]}: ${entry.timers[0].start} - ${entry.timers[entry.timers.length-2].end}`
-            });
-            console.log(username + " wants to know the current event");
-            message.channel.send(data.join("\n") + "\n***All times JST** :flag_jp:");
-        });
-    }
-    if(message.content == "!maint" || message.content == "!maintenance") {
-        readJson("json.json", function(err, data){
-            if(err) return errLogger(err);
-            data = currentEntries(data, "Maintenance");
-            data = data.map(entry =>{
-                return `${entry.title[0]}\n${entry.title[1]}: ${entry.timers[0].start} - ${entry.timers[0].end}`
-            });
-            console.log(username + " wants to know when the next maintenance is");
-            if (data.length > 0) {
-                message.channel.send(data.join("\n") + "\n***All times JST** :flag_jp:");
-            }
-            else message.channel.send("There are no scheduled maintenances.")
-        });
-    }
-    if(message.content == "!eventshops" || message.content == "!shops") {
-        readJson("json.json", function(err, data) {
-            if(err) return errLogger(err);
-            let events = currentEntries(data, "Event")
-            let timers = [].concat.apply([], events.map(event => event.timers));
-            timers = timers.filter(timer => timer.name.startsWith("Event Shop"));
-            data = timers.map(entry => {
-                return `${entry.name}: ${entry.start} - ${entry.end}`
-            });
-            console.log(username + " wants to know the current event shops")
-            message.channel.send(data.join("\n") + "\n***All times JST** :flag_jp:");
-        });
-    }
-    // todo
-    // other (stone sales, login bonus, campaigns, )
+    //todo
     // alert when stuff happens
     // beautify
 });
@@ -97,16 +172,14 @@ function errLogger(err) {
 }
 
 function currentEntries(entries, type) {
-    entries = entries.filter(entry => entry.type == type)
-    return entries.filter(entry => !isPast(dateConvert(entry.timers[0].end)))
+    entries = entries.filter(entry => entry.type == type);
+    return entries.filter(entry => !isPast(dateConvert(entry.timers[0].end)));
 }
 function isPast(date) {
     return date < new Date();
 }
 function dateConvert(date) {
-    let ret = new Date(date)
-    console.log(ret);
-    return ret;
+    return new Date(date);
 }
 
 const options = {
